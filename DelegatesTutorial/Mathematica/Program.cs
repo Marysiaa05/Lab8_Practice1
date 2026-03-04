c2fcf33ccdcee05a70416a5e51b8715ce8cd403f
Marysiaa05 <marysia.egm@gmail.com>
2026-03-04 17:50:10 +0100

    Finished Lab.

--- DelegatesTutorial/Mathematica/Program.cs
+++ DelegatesTutorial/Mathematica/Program.cs
@@ -1,21 +1,22 @@
-﻿Console.WriteLine("Hello, World");
+﻿var integrator = new Integrator();
 
+Console.WriteLine(integrator.Integrate(0, 1, 0.0001, x=> x * x));
+Console.WriteLine(integrator.Integrate(0, 1, 0.0001, x => Math.Sin(x)));
+public delegate double Integrable(double x);
 public class Integrator
 {
-    public double Integrate()
+    public double Integrate(double start, double end, double step, Func<double, double> function)
     {
-        var start = 0.0;
-        var end = 1.0;
-        var step = 0.1;
-
         var sum = 0.0;
 
         for (var x = start; x < end; x += step)
         {
-            var y = x * x;
+            var y = function(x);
             sum += y * step;
         }
 
         return sum;
     }
 }
+
+
